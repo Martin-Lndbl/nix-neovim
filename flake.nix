8277592c38939ea89258d03bc057a3d34141f544
martin-lndbl-GTL <lblsolutions@outlook.de>
2023-05-20 12:00:04 +0200

    flake init

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  }; 
+
+  outputs = { self, nixpkgs, ... }:
+    let 
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in 
+    {
+      neovim = ./default.nix;
+    };
+}
