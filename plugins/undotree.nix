30a10043d22a916ffe4a30be47a1debede7ea53b
martin-lndbl-GTL <lblsolutions@outlook.de>
2023-05-25 12:28:12 +0200

    fixed typo in undotree

--- plugins/undotree.nix
+++ plugins/undotree.nix
@@ -2,7 +2,7 @@
 ''
   let g:undotree_Windowlayout = 3
   let g:undotree_DiffpanelHeight = 10
-  let g:undotree_SetFoxusWhenToggle = 1
+  let g:undotree_SetFocusWhenToggle = 1
   let g:undotree_HighlightChangedText = 0
 
   nnoremap ${vimconf.leader}u :UndotreeToggle<CR>
